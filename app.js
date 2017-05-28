f519f60cdd7a8eddc3789c7340704b8e3c4c5824
avaschuk <andreivashchuk4@gmail.com>
2017-05-28 12:11:46 +0300

    add endpoint

---
+++ app.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+const jsonParser = bodyParser.json ({
+    type: 'application/json'
+});
+const urlencodedParser = bodyParser.urlencoded ({
+    extended: true,
+    type: 'application/x-www-form-urlencoded'
+});
+
+app.use(jsonParser);
+app.use(urlencodedParser);
+
+app.listen(process.env.PORT || 8100, function() {
+    console.log(`Listening hicapiher on port ${process.env.PORT || 8100}.`)
+});
+
+app.get('/', function (req, res) {
+   res.send('Hello world');
+});
